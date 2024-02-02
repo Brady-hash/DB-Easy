25211c98303ee1c87768f25318216bb790a3ec47
AndrueGage <trainer.andrue@gmail.com>
2024-02-01 20:58:20 -0600

    Created user and dog table. created indexjs

---
+++ models/Dog.js
@@ -0,0 +1,57 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Dog extends Model { }
+
+Dog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        breed: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        weight: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        spay_neuter: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        vaccinations: {
+            type: DataType.STRING,
+            allowNull: true
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscore: true,
+        modelName: 'dog',
+    }
+);
+
+module.exports = Dog;
