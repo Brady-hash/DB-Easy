9c591da80c8140497927c318f81185a59dd3bfbd
AndrueGage <trainer.andrue@gmail.com>
2024-02-07 19:59:22 -0600

    created logic (not done) for logging in

---
+++ views/public/js/login.js
@@ -0,0 +1,26 @@
+const loginFormHandler = async (event)=> {
+    event.preventDefault();
+
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({email, password}),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        if(response.ok) {
+            document.location.replace('/homepage');
+        } else {
+            alert('Could not load page')
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('')
+}
