472173b257809ef1358d5e59067359f00d707f76
AndrueGage <trainer.andrue@gmail.com>
2024-02-07 19:58:57 -0600

    updated port for server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
