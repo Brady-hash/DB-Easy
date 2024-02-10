a89b8d66f10087597c370f0e271265e7375b1b2f
AndrueGage <trainer.andrue@gmail.com>
2024-02-09 20:53:02 -0600

    login and signup form handlers

---
+++ public/js/login.js
@@ -0,0 +1,51 @@
+const loginFormHandler = async (event)=> {
+    event.preventDefault();
+
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({email, password}),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        if(response.ok) {
+            document.location.replace('/homepage');
+        } else {
+            alert(response.statusText)
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const firstName = document.querySelector('#firstname-signup').value.trim();
+    const lastName = document.querySelector('#lastname-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (firstname && lastName && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ firstName, lastName, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/login');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
